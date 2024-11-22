bf8f786f1657f784d305deda12a46a1834ba467b
Varuzh21 <varuzhan-grigoryan@inbox.ru>
2024-11-22 11:28:43 +0400

    add cors in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import messageRouters from './routes/message.route.js';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import { connectDB } from './lib/db.js';
+import cors from "cors";
 
 const app = express();
 
@@ -12,6 +13,12 @@ const PORT = process.env.PORT
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
 
 app.use("/api/auth", authRouters);
 app.use("/api/message", messageRouters);
